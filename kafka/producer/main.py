368e3b810e22b35d39785c7486b5b757faa0b5e1
saffronjam <own.emka@gmail.com>
2023-09-06 16:31:05 +0200

    add kafka docker scripts and a basic producer

---
+++ kafka/producer/main.py
@@ -0,0 +1,53 @@
+import json
+import time
+
+from sseclient import SSEClient as EventSource
+from kafka import KafkaProducer
+
+print("Starting producer...")
+
+# Create producer
+conn_attempts = 0
+max_conn_attempts = 10
+
+while conn_attempts < max_conn_attempts:
+    try:
+        if conn_attempts == max_conn_attempts:
+            print("Failed to connect to Kafka server... exiting")
+            exit()
+
+        producer = KafkaProducer(
+            bootstrap_servers='localhost:9092',  # Kafka server
+            value_serializer=lambda v: json.dumps(
+                v).encode('utf-8')  # json serializer
+        )
+
+        break
+    except KeyboardInterrupt:
+        print("process interrupted")
+        exit()
+    except Exception as e:
+        print(
+            f"Failed to connect to Kafka server: {str(e)} trying again ({conn_attempts}/{max_conn_attempts})")
+        conn_attempts += 1
+
+        time.sleep(3)
+
+
+print("Connected to Kafka server, starting to read stream...")
+
+# Read streaming event
+url = 'https://stream.wikimedia.org/v2/stream/recentchange'
+try:
+    for event in EventSource(url):
+        if event.event == 'message':
+            try:
+                change = json.loads(event.data)
+            except ValueError:
+                pass
+            else:
+                # Send msg to topic wiki-changes
+                producer.send('wiki-changes', change)
+
+except KeyboardInterrupt:
+    print("process interrupted")
