e6684931b53aa48a36e66729257d1f8aff82b4aa
saffronjam <own.emka@gmail.com>
2023-09-18 11:37:01 +0200

    remove reload from backend

--- backend/main.py
+++ backend/main.py
@@ -61,4 +61,4 @@ app = falcon.asgi.App()
 app.add_route('/v1/events', EventResource())
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080)
